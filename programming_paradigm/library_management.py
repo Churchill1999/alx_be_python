173c41bb962b3819d143efe5537624a5a95a4f8a
Churchill Otoo <churchillotoo@gmail.com>
2025-12-05 22:17:30 +0000

    Add library management system implementation

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,52 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        """Mark the book as returned (available)."""
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        """Check if the book is available."""
+        return not self._is_checked_out
+
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        """Add a Book object to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by title if available."""
+        for book in self._books:
+            if book.title == title:
+                return book.check_out()
+        return False  # Book not found
+
+    def return_book(self, title):
+        """Return a book by title."""
+        for book in self._books:
+            if book.title == title:
+                return book.return_book()
+        return False  # Book not found
+
+    def list_available_books(self):
+        """Print all available books."""
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
