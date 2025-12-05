b2f6fb35cfd8266d62e28447785d90617821dd84
Churchill Otoo <churchillotoo@gmail.com>
2025-12-05 23:51:24 +0000

    Implement Calculator class with static and class methods

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,7 +1,13 @@
-calculation_type = "Arithmetic Operations"
+class Calculator:
+    calculation_type = "Arithmetic Operations"
 
-@classmethod
-def multiply(cls, a, b):
-    print(f"Calculation type: {cls.calculation_type}")
-    return a * b
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
+
 
