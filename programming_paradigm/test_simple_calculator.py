73c198b647685fb620d1d9335185d0358569b36d
Churchill Otoo <churchillotoo@gmail.com>
2025-12-21 11:56:00 +0000

    Fix unit test names to match checker expectations

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,7 +1,9 @@
 import unittest
 from simple_calculator import SimpleCalculator
 
+
 class TestSimpleCalculator(unittest.TestCase):
+    """Unit tests for SimpleCalculator"""
 
     def setUp(self):
         self.calc = SimpleCalculator()
@@ -9,24 +11,19 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
-        self.assertEqual(self.calc.add(0, 0), 0)
 
     def test_subtraction(self):
-        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(0, 5), -5)
-        self.assertEqual(self.calc.subtract(-3, -3), 0)
 
-    def test_multiply(self):
-        self.assertEqual(self.calc.multiply(2, 4), 8)
-        self.assertEqual(self.calc.multiply(-1, 5), -5)
-        self.assertEqual(self.calc.multiply(0, 10), 0)
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertEqual(self.calc.divide(-6, 3), -2)
-        # division by zero should return None
-        self.assertIsNone(self.calc.divide(5, 0))
+        self.assertIsNone(self.calc.divide(10, 0))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
-
